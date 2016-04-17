2406805e1975365fcdca7878777466eeb16d7184
Hoang Do <hoangdo.dh94@gmail.com>
2016-04-17 18:52:04 +0700

    update

--- app/src/main/java/hcmut/cse/bookslover/LoginActivity.java
+++ app/src/main/java/hcmut/cse/bookslover/LoginActivity.java
@@ -11,7 +11,7 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        setTitle("Đăng ký");
+        setTitle("Đăng Nhập");
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
